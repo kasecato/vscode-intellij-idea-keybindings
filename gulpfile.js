93d18f83040a634356caf14c6b9931cd97d25716
k--kato <keisuke.k.kato@gmail.com>
2016-04-04 11:46:07 +0900

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@ var gulp = require('gulp');
 var rename = require("gulp-rename"); 
 var stripJsonComments = require('gulp-strip-json-comments');
  
-gulp.task('build', function () {
-    return gulp.src('src/package-with-comment.json')
+gulp.task('default', function () {
+    return gulp.src('./src/package-with-comment.json')
         .pipe(stripJsonComments())
         .pipe(rename("./package.json"))
         .pipe(gulp.dest('./'));
 });
+
+
+gulp.task('watch', function(){
+    gulp.watch('./src/package-with-comment.json', ['build']);
+});
+
+gulp.task('build', ['default', 'watch']);
