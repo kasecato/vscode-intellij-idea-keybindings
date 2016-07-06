1ab654468eccb427944fec937dbd6a91b637bd5f
Keisuke KATO <keisuke.k.kato@gmail.com>
2016-07-06 10:56:35 +0900

    fixed #5 Line comment not working with numpad (#6)
    * fixed #5
    
    * fixed #5

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var rename = require("gulp-rename"); 
 var stripJsonComments = require('gulp-strip-json-comments');
+var prettify = require('gulp-jsbeautifier');
  
 gulp.task('default', function () {
     return gulp.src('./src/package-with-comment.json')
         .pipe(stripJsonComments())
+        .pipe(prettify())
         .pipe(rename("./package.json"))
         .pipe(gulp.dest('./'));
 });
